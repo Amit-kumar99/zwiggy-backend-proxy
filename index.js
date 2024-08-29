a87f74b1baf4788255c93b194dd52e603fc03714
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-08-29 10:25:13 +0530

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
+
+app.use(cors());
 
 // Health check route
 app.get('/health', (req, res) => {
